51a8e9235faa29af1008847642c47b490779242e
satrio <satrio.ook.2008@gmail.com>
2024-10-18 00:47:11 +0700

    Week 4

---
+++ start.sh
@@ -0,0 +1,8 @@
+set -eu 
+export PYTHONUNBUFFERED=true 
+# Pastikan pip terinstal 
+pip3 install --upgrade pip 
+# Menginstal dependensi langsung tanpa virtual environment 
+pip3 install -r requirements.txt 
+# Menjalankan aplikasi 
+python3 app.py
